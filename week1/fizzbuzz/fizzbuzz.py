91076c4d596e6b9e29640ece5fdf87b172285cd6
kara temple <you@example.com>
2021-09-29 18:01:01 -0400

    firsttest

---
+++ week1/fizzbuzz/fizzbuzz.py
@@ -0,0 +1,9 @@
+def findAnswer(a):
+    if a == 1:
+        return 1
+    elif a == 2:
+        return 2
+
+print(findAnswer(1))
+
+
---
+++ week1/fizzbuzz/fizzbuzzunittest.py
@@ -0,0 +1,23 @@
+# can call fizz buzz 
+# gets "1" when 1 is passed in
+# gets 2 when 2 is passed in
+#In the fizzbuzzunittest.py file, write a code that should achieve 
+# above tests through failing test, all the way to refractor. 
+
+import unittest
+import fizzbuzz
+
+class FirstTest(unittest.TestCase):
+    def test_one_stringOne(self):
+        fizzbuzz.findAnswer(1) == 1
+        
+if __name__ == '__main__':
+    unittest.main()
+
+   
+  
+
+
+    
+
+    
