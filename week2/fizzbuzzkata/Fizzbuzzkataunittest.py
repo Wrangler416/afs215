292b6ff58cf11274037a8d9a9bfcd536fd7a39a3
kara temple <you@example.com>
2021-10-09 16:05:52 -0400

    fizzbuzz

--- week2/fizzbuzzkata/Fizzbuzzkataunittest.py
+++ week2/fizzbuzzkata/Fizzbuzzkataunittest.py
@@ -5,6 +5,34 @@ import fizzbuzz
 class Test_FizzBuzz(unittest.TestCase):
     
         def test_One_Fizzbuzz(self):
-                
+                result = fizzbuzz.findAnswer(1)
+                assert result == 1
 
-            
+        def test_Two_Fizzbuzz(self):
+                result = fizzbuzz.findAnswer(2) 
+                assert result == 2
+
+        def test_Three_Fizzbuzz(self):
+                result = fizzbuzz.findAnswer(3)   
+                assert result == "fizz"  
+
+        def test_Four_Fizzbuzz(self):
+                result = fizzbuzz.findAnswer(5)
+                assert result == "buzz"
+
+        def test_Five_Fizzbuzz(self):
+                result = fizzbuzz.findAnswer(6)
+                assert result == "fizz"
+        
+        def test_Six_Fizzbuzz(self):
+                result = fizzbuzz.findAnswer(10)
+                assert result == "buzz"
+
+        def test_Seven_Fizzbuzz(self):
+                result = fizzbuzz.findAnswer(15)
+                assert result == "fizzbuzz"
+
+    
+if __name__ == '__main__':
+    unittest.main()
+    
--- week2/fizzbuzzkata/fizzbuzz.py
+++ week2/fizzbuzzkata/fizzbuzz.py
@@ -1,21 +1,29 @@
-for i in range(60):
+def findAnswer(i):
+        if i == 1:
+            return 1 
+         
+        if i == 2:
+            return 2
+        
+        if i % 5 == 0 and i % 3 == 0:
+            return "fizzbuzz"
 
-    if i == 1:
-        print("1")
-    
-    elif i == 2:
-        print("2")
+        if i % 3 == 0:
+            return "fizz"
+        
+        elif i % 5 == 0:
+            return "buzz"
 
-    elif i % 3 == 0:
-        print("fizz")
-        continue
+        
 
-    elif i % 5 == 0:
-        print("buzz")
-        continue
+print(findAnswer(1))
+print(findAnswer(2))
+print(findAnswer(3))
+print(findAnswer(6))
+print(findAnswer(15))
+print(findAnswer(100))
+print(findAnswer(15))
 
-    elif i % 5 and i % 5 == 0:
-        print("fizzbuzz")
-        continue
+        
 
-    print(i)
+
