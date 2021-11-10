20ff4a2e61ee39d28a53e8a79ff31fc13b4b1089
Wrangler416 <kara.temple@bryanuniversity.edu>
2021-11-10 11:03:24 -0400

    fizzbuzznode

--- week6/fizzbuzz/fizzbuzz.js
+++ week6/fizzbuzz/fizzbuzz.js
@@ -1,13 +1,21 @@
 module.exports = {
-    FizzBuzz: function(){
+    One: function(){
        return 1
     },
     FizzBuzzTwo: function(){
         return 2
     },
 
-    Fizz:  function(a){
-       return a + "Fizz"
+    Fizz:  function(){
+       return "Fizz"
+    },
+    Buzz: function(){
+       return "Buzz"
+    },
+
+    FizzBuzz: function(a){
+       if(a % 3 == 0 && a % 5 == 0)
+       return "FizzBuzz"
     }
  }
 
--- week6/fizzbuzz/test.js
+++ week6/fizzbuzz/test.js
@@ -1,20 +1,44 @@
 const assert = require('chai').assert
-const FizzBuzz = require('./fizzbuzz').FizzBuzz
+const One = require('./fizzbuzz').One
 const FizzBuzzTwo = require('./fizzbuzz').FizzBuzzTwo
 const Fizz = require('./fizzbuzz').Fizz
-
+const Buzz = require('./fizzbuzz').Buzz
+const FizzBuzz = require('./fizzbuzz').FizzBuzz
 
 describe("FizzBuzz", function(){
-    it('gets one when one is passed on and two if two is passed in', function(){
-        assert.equal(FizzBuzz(), 1)
+    it('gets one when one is passed', function(){
+        let result = One()
+        assert.equal(result, 1)
     })
 
-    it('gets two when 2 passed in',function(){
-        assert.equal(FizzBuzzTwo(), 2)
+    it('gets two when two passed',function(){
+        let result = FizzBuzzTwo()
+        assert.equal(result, 2)
+    })
+
+    it('gets fizz when 3 is passed', function(){
+        let result = Fizz(3)
+        assert.equal(result, "Fizz")
+    })
+
+    it('gets buzz when 5 is passed', function(){
+        let result = Buzz(5)
+        assert.equal(result, "Buzz")
+    })
+
+    it('gets fizz when 6 passed', function(){
+        let result = Fizz(6)
+        assert.equal(result, "Fizz")
     })
 
     it('gets fizz when 3 passed', function(){
-        assert.equal(Fizz(), 3)
+        let result = Buzz(10)
+        assert.equal(result, "Buzz")
+    })
+
+    it('gets FizzBuzz when multiple of 3 and 5', function(){
+        let result = FizzBuzz(15)
+        assert.equal(result, "FizzBuzz")
     })
 })
 
